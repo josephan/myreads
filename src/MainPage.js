f7d8db553aeee05a8464eda124c9afd45302fec9
Joseph An <an.joseph891@gmail.com>
2017-10-24 00:19:12 -0400

    Add BookAPI and initialize state with reduced list

--- src/MainPage.js
+++ src/MainPage.js
@@ -2,7 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import BookShelf from './BookShelf.js';
 
+import * as BooksAPI from './BookAPI.js'
+
 class MainPage extends React.Component {
+  state = {
+    currentlyReading: [],
+    wantToRead: [],
+    read: [],
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      const groupedBooks = books.reduce((acc, item) => {
+        let key = item.shelf;
+        acc[key] = acc[key] || [];
+        acc[key].push(item);
+        return acc;
+      }, {});
+      this.setState({ ...groupedBooks })
+    })
+  }
+
   render() {
     return (
       <div>
