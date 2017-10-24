ee5bbfb4c23a1f77021f9061a5ec858166f24a16
Joseph An <an.joseph891@gmail.com>
2017-10-23 23:45:26 -0400

    Remove generated template, add normalize.css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
